2d04fc0cd3816205823c7e0e01fcd3e40cee41eb
dIOGOLOC <locdiogo@gmail.com>
2018-06-04 15:17:19 -0300

    some changes in migration_410_660.py

--- MIGRATION_NON_PLANE_program/migration_410_660.py
+++ MIGRATION_NON_PLANE_program/migration_410_660.py
@@ -1,71 +1,19 @@
 #!/usr/bin/python -u
 """
-[Advice: run this script using python with unbuffered output:
-`python -u crosscorrelation.py`]
-This script reads seismic waveform data from a set of stations, and
-calculates the cross-correlations between all pairs of stations. The
-data (in miniseed format) must be located in folder *MSEED_DIR*. The
-stations information (coordinates, instrument response) can be read
-from dataless seed files (if *USE_DATALESSPAZ* = True) located in
-folder *DATALESS_DIR*, and/or stationXML files (if *USE_STATIONXML* =
-True) located in folder *STATIONXML_DIR*. Note that two different
-stations MUST HAVE DIFFERENT NAMES, even if they do not belong to
-the same network. Also, one given station cannot have several
-sets of coordinates: if so, it will be skipped.
-In the current version of the program, miniseed files MUST be
-organized inside their directory as:
-<year>-<month>/<network>.<station>.<channel>.mseed, e.g.:
-1988-10/BL.JFOB.BHZ.mseed
-So, there is one sub-directory per month, and inside it, one miniseed
-file  per month and per station.
-The implemented algorithm follows the lines of Bensen et al.,
-"Processing seismic ambient noise data to obtain reliable broad-band
-surface wave dispersion measurements", Geophys. J. Int. (2007).
-The procedure consists in stacking daily cross-correlations between
-pairs of stations, from *FIRSTDAY* to *LASTDAY* and, in each given day,
-rejecting stations whose data fill is < *MINFILL*. Define a subset of
-stations to cross-correlate in *CROSSCORR_STATIONS_SUBSET* (or let it
-empty to cross-correlate all stations). Define a list of locations to
-skip in *CROSSCORR_SKIPLOCS*, if any. The cross-correlations are
-calculated between -/+ *CROSSCORR_TMAX* seconds.
-Several pre-processing steps are applied to the daily seismic waveform
-data, before the daily cross-correlation is calculated and stacked:
-(1) removal of the instrument response, the mean and the trend;
-(2) band-pass filter between *PERIODMIN* and *PERIODMAX* sec
-(3) down-sampling to sampling step = *PERIOD_RESAMPLE* sec
-(4) time-normalization:
-    - if *ONEBIT_NORM* = False, normalization of the signal by its
-      (smoothed) absolute amplitude in the earthquake period band,
-      defined as *PERIODMIN_EARTHQUAKE* - *PERIODMIN_EARTHQUAKE* sec.
-      The smoothing window is *PERIODMAX_EARTHQUAKE* / 2;
-    - if *ONEBIT_NORM* = False, one-bit normalization, wherein
-      only the sign of the signal is kept (+1 or -1);
-(5) spectral whitening of the Fourier amplitude spectrum: the Fourier
-    amplitude spectrum of the signal is divided by a smoothed version
-    of itself. The smoonthing window is *WINDOW_FREQ*.
-Note that all the parameters mentioned above are defined in the
-configuration file.
-When all the cross-correlations are calculated, the script exports
-several files in dir *CROSSCORR_DIR*, whose name (without extension)
-is:
-xcorr[_<stations of subset>]_<first year>-<last year>[_1bitnorm] ...
-      _[datalesspaz][+][xmlresponse][_<suffix>]
-where <suffix> is provided by the user. For example:
-"xcorr_1996-2012_xmlresponse"
-The files, depending on their extension, contain the following data:
-- .pickle       = set of all cross-correlations (instance of
-                  pscrosscorr.CrossCorrelationCollection) exported in binary
-                  format with module pickle;
-- .txt          = all cross-correlations exported in ascii format
-                  (one column per pair);
-- .stats.txt    = general information on cross-correlations in ascii
-                  format: stations coordinates, number of days, inter-
-                  station distance etc.
-- .stations.txt = general information on the stations: coordinates,
-                  nb of cross-correlations in which it appears, total
-                  nb of days it has been cross-correlated etc.
-- .png          = figure showing all the cross-correlations (normalized to
-                  unity), stacked as a function of inter-station distance.
+This script reads receiver function data from a set of stations, and
+calculates the ray path between all pairs station-event. The data MUST be 
+located in some root folder (RF_DIR). The station-event informations 
+(coordinates) MUST be in the header of the files. In the current version 
+of the program, files should be organized inside their directory as you prefer, 
+but they need to finish with some extension name (RF_EXT), e.g.:
+*.mseed, *.sac, *.itr, *.eqr
+The implemented algorithm follows the lines of Gao and Liu (2014),
+"Imaging mantle discontinuities using multiply-reflected P-to-S
+conversions", Earth and Planetary Science Letters 402 (2014) 99–106.
+Here we utilize both the P-to-S converted phase (Pds) and the multiply reflected 
+and converted phase (Ppds) at the discontinuities to simultaneously determine the 
+depth of mantle discontinuities and velocity anomalies in the overlying layer. 
+Note that all the parameters are defined in the configuration file.
 """
 
 from time_scripts import time_P_Pds
