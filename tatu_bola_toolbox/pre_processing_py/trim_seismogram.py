a6aead29360aa3a60dc51f712a44a73a50152eaf
dIOGOLOC <locdiogo@gmail.com>
2018-09-18 10:15:03 -0300

    Renaming receiver function migration.

--- tatu_bola_toolbox/pre_processing_py/trim_seismogram.py
+++ tatu_bola_toolbox/pre_processing_py/trim_seismogram.py
@@ -6,17 +6,19 @@ Function to trim raw sac files
 import os
 import obspy as op
 from obspy.taup import TauPyModel
+import numpy as np
 
 from parameters_py.config import (
 					DIR_SAC,DIR_EVENT,STA_CSV_FILE,OUTPUT_JSON_FILE_DIR,TAUPY_MODEL,
 					EV_GCARC_MIN,EV_GCARC_MAX,EV_MAGNITUDE_MB,CUT_BEFORE_P,CUT_AFTER_P,KCMPNM_N,
 					KCMPNM_E,KCMPNM_Z,knetwk,NAME_SUFFIX_N,NAME_SUFFIX_E,NAME_SUFFIX_Z,MP_PROCESSES
 				   )
+
 #Function to cut each file in each folder from every event:
 
 def cut_data_by_event(kstnm,stla,stlo,ev_timeUTC,ev_julday,ev_year,ev_month,ev_day,ev_hour,ev_minute,ev_second,ev_microsecond,ev_lat,ev_long,ev_depth,ev_mag):
 	if os.path.isdir(DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday) == True:
-		try:
+		#try:
 			#Calculating distance, azimuth and backazimuth
 			dist,az,baz = op.geodetics.gps2dist_azimuth(ev_lat,ev_long,stla,stlo)
 			gcarc = op.geodetics.kilometer2degrees(dist/1000)
@@ -85,6 +87,125 @@ def cut_data_by_event(kstnm,stla,stlo,ev_timeUTC,ev_julday,ev_year,ev_month,ev_d
 						}
 
 				sacHHZ = op.io.sac.sactrace.SACTrace(data=HHZ[0].data, **headerHHZ)
+				sacHHZ.write(event_directory+'/'+knetwk+'.'+kstnm+'.'+'{:04}'.format(starttime.year)+'.'+'{:03}'.format(starttime.julday)+'.'+'{:02}'.format(starttime.hour)+'.'+'{:02}'.format(starttime.minute)+'.'+'{:02}'.format(starttime.second)+'.'+'{:03}'.format(starttime.microsecond)[:3]+'.'+NAME_SUFFIX_Z)
+				
+			#return 'Data OK with this event = '+DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday
+		#except: 
+			#return 'Data error with this event = '+DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday
+		
+	return 'No data for this event = '+DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday
+
+#Function to cut each file in each folder from every event without PP wave:
+
+
+def cut_NO_PP_data_by_event(kstnm,stla,stlo,ev_timeUTC,ev_julday,ev_year,ev_month,ev_day,ev_hour,ev_minute,ev_second,ev_microsecond,ev_lat,ev_long,ev_depth,ev_mag):
+	if os.path.isdir(DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday) == True:
+		try:
+			#Calculating distance, azimuth and backazimuth
+			dist,az,baz = op.geodetics.gps2dist_azimuth(ev_lat,ev_long,stla,stlo)
+			gcarc = op.geodetics.kilometer2degrees(dist/1000)
+			if EV_GCARC_MIN <= gcarc <= EV_GCARC_MAX and EV_MAGNITUDE_MB <= ev_mag:
+				print('Current Directory = '+DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday)		
+				os.chdir(DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday)
+				#Calculating ray parameter
+				model = TauPyModel(model=TAUPY_MODEL)
+				arrivals = model.get_travel_times(source_depth_in_km=ev_depth, distance_in_degree=gcarc, phase_list=["P"])
+				arr = arrivals[0]
+				#Reference time
+				starttime = (op.UTCDateTime(ev_timeUTC)+arr.time)-CUT_BEFORE_P
+				endtime = (op.UTCDateTime(ev_timeUTC)+arr.time)+CUT_AFTER_P	
+
+				#Creating Event Directory 
+				event_directory = DIR_EVENT+kstnm+'/'+'{:04}'.format(starttime.year)+'/'+'{:03}'.format(starttime.julday)+'/'+'{:04}'.format(starttime.year)+'.'+'{:03}'.format(starttime.julday)+'.'+'{:02}'.format(starttime.hour)+'.'+'{:02}'.format(starttime.minute)+'.'+'{:02}'.format(starttime.second)+'.'+'{:03}'.format(starttime.microsecond)[:3]			
+				os.makedirs(event_directory, exist_ok=True)
+				#########################################################################################################################################################
+				#Component E
+				HHX = op.read('*.'+NAME_SUFFIX_E)
+				HHX.trim(starttime,endtime)
+
+				arrivalsPP =  model.get_travel_times(source_depth_in_km=ev_depth, distance_in_degree=gcarc, phase_list=["PP"])
+				arrPP = arrivalsPP[0]	
+
+				time_PP = arrPP.time - arr.time + CUT_BEFORE_P
+				data_HHX = []
+		
+				for k,l in enumerate(HHX[0].times()):
+					if time_PP - 30.0 <= l <= time_PP - 5.0:
+						data_HHX.append(HHX[0].data[k] * np.exp(-0.2*(l - time_PP + 30.0)))
+					elif time_PP - 5.0 <= l <= time_PP + 5.0: 
+						data_HHX.append(HHX[0].data[k] * 0.0)
+					elif time_PP + 5.0 <= l <= time_PP + 30.0: 
+						data_HHX.append(HHX[0].data[k] * np.exp(-0.2*(-l + time_PP + 30.0)))
+					else:
+						data_HHX.append(HHX[0].data[k])
+				headerHHX = {
+						'kstnm': kstnm, 'kcmpnm': KCMPNM_E, 'stla': float(stla), 'stlo': float(stlo), 'evdp': float(ev_depth),  
+						'evla': float(ev_lat), 'evlo': float(ev_long), 'mag': float(ev_mag), 'nzhour': int(starttime.hour),
+						'nzjday': int(starttime.julday), 'nzmin': int(starttime.minute),'nzmsec': int('{:03}'.format(starttime.microsecond)[:3]), 'nzsec': int(starttime.second), 
+						'nzyear': int(starttime.year),'cmpaz': 90.0, 'cmpinc': 90.0,'dist': float(dist/1000), 'gcarc': float(gcarc), 
+						'az': float(az), 'baz': float(baz), 'user8': float(arr.ray_param/6371),'o':float(CUT_BEFORE_P),'delta':HHX[0].stats.delta
+						}
+
+				sacHHX = op.io.sac.sactrace.SACTrace(data=np.array(data_HHX), **headerHHX)
+				sacHHX.write(event_directory+'/'+knetwk+'.'+kstnm+'.'+'{:04}'.format(starttime.year)+'.'+'{:03}'.format(starttime.julday)+'.'+'{:02}'.format(starttime.hour)+'.'+'{:02}'.format(starttime.minute)+'.'+'{:02}'.format(starttime.second)+'.'+'{:03}'.format(starttime.microsecond)[:3]+'.'+NAME_SUFFIX_E)
+				
+				#########################################################################################################################################################
+				#Component N
+				HHY = op.read('*.'+NAME_SUFFIX_N)
+				HHY.trim(starttime,endtime)
+
+				data_HHY = []
+		
+				for k,l in enumerate(HHY[0].times()):
+					if time_PP - 30.0 <= l <= time_PP - 5.0:
+						data_HHY.append(HHY[0].data[k] * np.exp(-0.2*(l - time_PP + 30.0)))
+					elif time_PP - 5.0 <= l <= time_PP + 5.0: 
+						data_HHY.append(HHY[0].data[k] * 0.0)
+					elif time_PP + 5.0 <= l <= time_PP + 30.0: 
+						data_HHY.append(HHY[0].data[k] * np.exp(-0.2*(-l + time_PP + 30.0)))
+					else:
+						data_HHY.append(HHY[0].data[k])
+
+				headerHHY = {
+						'kstnm': kstnm, 'kcmpnm': KCMPNM_N, 'stla': float(stla), 'stlo': float(stlo), 'evdp': float(ev_depth),  
+						'evla': float(ev_lat), 'evlo': float(ev_long), 'mag': float(ev_mag), 'nzhour': int(starttime.hour),
+						'nzjday': int(starttime.julday), 'nzmin': int(starttime.minute), 'nzmsec': int('{:03}'.format(starttime.microsecond)[:3]),'nzsec': int(starttime.second),
+						'nzyear': int(starttime.year),	'cmpaz': 0.0, 'cmpinc': 90.0, 'dist': float(dist/1000), 'gcarc': float(gcarc), 
+						'az': float(az), 'baz': float(baz), 'user8': float(arr.ray_param/6371),'o':float(CUT_BEFORE_P),'delta':HHY[0].stats.delta
+						}
+
+				sacHHY = op.io.sac.sactrace.SACTrace(data=np.array(data_HHY), **headerHHY)
+				sacHHY.write(event_directory+'/'+knetwk+'.'+kstnm+'.'+'{:03}'.format(starttime.year)+'.'+'{:03}'.format(starttime.julday)+'.'+'{:02}'.format(starttime.hour)+'.'+'{:02}'.format(starttime.minute)+'.'+'{:02}'.format(starttime.second)+'.'+'{:03}'.format(starttime.microsecond)[:3]+'.'+NAME_SUFFIX_N)
+
+
+				#########################################################################################################################################################
+				#Component Z
+
+				HHZ = op.read('*.'+NAME_SUFFIX_Z)
+				HHZ.trim(starttime,endtime)
+
+				data_HHZ = []
+					
+				for k,l in enumerate(HHZ[0].times()):
+					if time_PP - 30.0 <= l <= time_PP - 5.0:
+						data_HHZ.append(HHZ[0].data[k] * np.exp(-0.2*(l - time_PP + 30.0)))
+					elif time_PP - 5.0 <= l <= time_PP + 5.0: 
+						data_HHZ.append(HHZ[0].data[k] * 0.0)
+					elif time_PP + 5.0 <= l <= time_PP + 30.0: 
+						data_HHZ.append(HHZ[0].data[k] * np.exp(-0.2*(-l + time_PP + 30.0)))
+					else:
+						data_HHZ.append(HHZ[0].data[k])
+
+				headerHHZ = {
+						'kstnm': kstnm, 'kcmpnm': KCMPNM_Z, 'stla': float(stla), 'stlo': float(stlo), 'evdp': float(ev_depth),  
+						'evla': float(ev_lat), 'evlo': float(ev_long), 'mag': float(ev_mag), 'nzhour': float(ev_hour),
+						'nzjday': int(starttime.julday), 'nzmin': int(starttime.minute), 'nzmsec': int('{:03}'.format(starttime.microsecond)[:3]),'nzsec': int(starttime.second),
+						'nzyear': int(starttime.year),	'cmpaz': 0.0, 'cmpinc': 0.0, 'dist': float(dist/1000), 'gcarc': float(gcarc), 
+						'az': float(az), 'baz': float(baz), 'user8': float(arr.ray_param/6371),'o':float(CUT_BEFORE_P),'delta':HHZ[0].stats.delta
+						}
+
+				sacHHZ = op.io.sac.sactrace.SACTrace(data=np.array(data_HHZ), **headerHHZ)
+				print(sacHHZ)
 				sacHHZ.write(event_directory+'/'+knetwk+'.'+kstnm+'.'+'{:04}'.format(starttime.year)+'.'+'{:03}'.format(starttime.julday)+'.'+'{:02}'.format(starttime.hour)+'.'+'{:02}'.format(starttime.minute)+'.'+'{:02}'.format(starttime.second)+'.'+'{:03}'.format(starttime.microsecond)[:3]+'.'+NAME_SUFFIX_Z)
 				
 			return 'Data OK with this event = '+DIR_SAC+kstnm+'/'+ev_year+'/'+ev_julday
---
+++ tatu_bola_toolbox/trim_NO_PP_data.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python -u
+"""
+This script reads sac data from a set of stations and trim the data.
+"""
+import numpy as np
+import obspy
+import os
+from obspy.taup import TauPyModel
+from obspy.geodetics import kilometer2degrees
+import json
+from multiprocessing import Pool
+
+
+# =====================================
+# Importing trim data script_py 
+# =====================================
+
+from pre_processing_py.trim_seismogram import cut_NO_PP_data_by_event
+
+# ==================================================
+#  Importing some parameters from configuration file 
+# ==================================================
+
+from parameters_py.config import (
+					DIR_SAC,DIR_EVENT,STA_CSV_FILE,OUTPUT_JSON_FILE_DIR,TAUPY_MODEL,
+					EV_GCARC_MIN,EV_GCARC_MAX,EV_MAGNITUDE_MB,CUT_BEFORE_P,CUT_AFTER_P,KCMPNM_N,
+					KCMPNM_E,KCMPNM_Z,knetwk,NAME_SUFFIX_N,NAME_SUFFIX_E,NAME_SUFFIX_Z,MP_PROCESSES
+				   )
+
+
+
+# ==================================
+#  Function to call cut data script
+# ==================================
+
+def parallel_trim_data(kstnm,stla,stlo,ev_timeUTC,ev_julday,ev_year,ev_month,ev_day,ev_hour,ev_minute,ev_second,ev_microsecond,	ev_lat,ev_long,ev_depth,ev_mag):
+			
+	station_data_result = cut_NO_PP_data_by_event(kstnm=kstnm,stla=stla,stlo=stlo,ev_timeUTC=ev_timeUTC,ev_julday=ev_julday,ev_year=ev_year,ev_month=ev_month,
+						ev_day=ev_day,ev_hour=ev_hour,ev_minute=ev_minute,ev_second=ev_second,ev_microsecond=ev_microsecond,
+						ev_lat=ev_lat,ev_long=ev_long,ev_depth=ev_depth,ev_mag=ev_mag)
+
+	return station_data_result
+
+
+# ============================================
+#  Importing station dictionary from JSON file 
+# ============================================
+
+print('Looking for STATIONS data in JSON file in '+OUTPUT_JSON_FILE_DIR)
+print('\n')
+
+filename_STA = OUTPUT_JSON_FILE_DIR+'STA_dic.json'
+
+sta_dic = json.load(open(filename_STA))
+
+kstnm = sta_dic['KSTNM']
+stla = sta_dic['STLA']
+stlo = sta_dic['STLO']
+stel = sta_dic['STEL']
+sensor_keys = sta_dic['SENSOR_KEYS']
+datalogger_keys = sta_dic['DATALOGGER_KEYS']
+
+for i in kstnm:
+	print('Station = '+i)
+print('\n')
+# ============================================
+#  Importing Event dictionary from JSON file 
+# ============================================
+
+
+print('\n')
+print('Looking for Events data in JSON file in '+OUTPUT_JSON_FILE_DIR)
+print('\n')
+
+filename_STA = OUTPUT_JSON_FILE_DIR+'EVENT_dic.json'
+
+event_dic = json.load(open(filename_STA))
+
+ev_year = event_dic['ev_year']
+ev_month = event_dic['ev_month']
+ev_julday = event_dic['ev_julday']
+ev_day = event_dic['ev_day']
+ev_hour = event_dic['ev_hour']
+ev_minute = event_dic['ev_minute']
+ev_second = event_dic['ev_second']
+ev_microsecond = event_dic['ev_microsecond']
+ev_timeUTC = event_dic['ev_timeUTC']
+evla = event_dic['evla']
+evlo = event_dic['evlo']
+evdp = event_dic['evdp']
+mag = event_dic['mag']
+
+print('Number of events = '+str(len(mag)))
+print('\n')
+# ==============================
+#  Creating stations Input lists
+# ==============================
+
+print('Creating stations input lists')
+print('\n')
+
+input_list = [[]]*len(kstnm)
+for i,j in enumerate(kstnm):
+	print('Creating input list: '+j)
+	print('\n')
+	input_list[i] = [
+			[kstnm[i],stla[i],stlo[i],ev_timeUTC[k],ev_julday[k],ev_year[k],ev_month[k],ev_day[k],ev_hour[k],ev_minute[k],ev_second[k],ev_microsecond[k],evla[k],evlo[k],evdp[k],mag[k]]
+			 for k,l in enumerate(ev_year)
+			]
+print('\n')
+
+# ==============
+#  Cutting data
+# ==============
+
+print('Cutting data and saving events folder for each station')
+print('\n')
+
+pool_trim = Pool(MP_PROCESSES)
+error_station_data_dic = {'kstnm':[],'error_dir':[]}
+for i,j in enumerate(input_list):
+	print('Station: '+kstnm[i])
+	trim_data_result = pool_trim.starmap(parallel_trim_data, j)
+	
+	error_station_data_dic['error_dir'].append(pool_trim.starmap(parallel_trim_data, j))
+	error_station_data_dic['kstnm'].append(kstnm[i])
+
+pool_trim.close()
+
+
+#Saving stations data error in JSON file
+print('Saving stations data error in JSON file')
+with open(OUTPUT_JSON_FILE_DIR+'STA_data_error.json', 'w') as fp:
+	json.dump(error_station_data_dic, fp)
+
+print('Cutting finished!')
